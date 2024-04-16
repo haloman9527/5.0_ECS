3e4ae6da4522eed972000e644078494d4810802e
haloman9527 <haloman9527@qq.com>
2024-04-16 19:06:58 +0800

    更新

--- Runtime/Archytype.cs
+++ Runtime/Archytype.cs
@@ -1,45 +1,91 @@
-﻿using CZToolKit.UnsafeEx;
+﻿using System;
 using Unity.Collections;
 
 namespace CZToolKit.ECS
 {
-    public unsafe struct Archetype
+    public unsafe struct Archetype : IDisposable
     {
-        public TypeInfo* types;
-        public int typesCount;
+        public const int DEFAULT_CHUNKS_LENGTH = 4;
 
+        public TypeInfo* sortedTypes;
+        public int typesCount;
+        public int archetypeHash;
+        public int chunkSize;
+        public NativeParallelHashMap<int, int> typeInArchetypeIndexMap;
         public NativeArray<Chunk> chunks;
 
-        public int hash;
-
-        public Archetype(TypeInfo* types, int count)
+        public Archetype(TypeInfo* sortedTypes, int count)
         {
-            this.types = types;
+            this.sortedTypes = sortedTypes;
             this.typesCount = count;
-            
-            this.chunks = new NativeArray<Chunk>();
 
-            this.hash = 0;
+            this.archetypeHash = 0;
+            this.chunkSize = 0;
+
+            this.typeInArchetypeIndexMap = new NativeParallelHashMap<int, int>(count, Allocator.Persistent);
+            this.chunks = new NativeArray<Chunk>(DEFAULT_CHUNKS_LENGTH, Allocator.Persistent);
+
             for (int i = 0; i < count; i++)
             {
-                this.hash ^= (types + i)->id;
+                var typeInfo = (sortedTypes + i);
+                this.archetypeHash ^= typeInfo->id;
+                this.chunkSize += typeInfo->componentSize;
+                this.typeInArchetypeIndexMap[typeInfo->id] = i;
             }
+        }
+
+        public int GetTypeInArchetypeIndex(int typeId)
+        {
+            if (!typeInArchetypeIndexMap.TryGetValue(typeId, out var typeInArchetypeIndex))
+            {
+                return -1;
+            }
+
+            return typeInArchetypeIndex;
+        }
+
+        public int GetTypeInArchetypeIndex(TypeInfo typeInfo)
+        {
+            var typeId = typeInfo.id;
+            return GetTypeInArchetypeIndex(typeId);
+        }
+
+        public int GetTypeInArchetypeIndex(Type type)
+        {
+            var typeId = TypeManager.GetTypeId(type);
+            return GetTypeInArchetypeIndex(typeId);
+        }
+
+        public void Dispose()
+        {
+            typeInArchetypeIndexMap.Dispose();
+            chunks.Dispose();
         }
     }
 
     public unsafe struct Chunk
     {
-        public const int kChunkSize = 16 * 1024;
-        
+        public const int CHUNK_SIZE = 16 * 1024;
+
         public Archetype* archetype;
+        
         public int capacity;
         public int count;
+
+        public Chunk(Archetype* archetype)
+        {
+            this.archetype = archetype;
+            this.capacity = CHUNK_SIZE / (archetype->chunkSize + sizeof(Entity));
+            this.count = 0;
+        }
     }
 
     public unsafe class TypeInArchetype
     {
         public Archetype* archetype;
-        public int typeIndex;
+        public int typeId;
+        public int indexInChunk;
+        public int offsetInChunk;
     }
 
     public unsafe class EntityInChunk
